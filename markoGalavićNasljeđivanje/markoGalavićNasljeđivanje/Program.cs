5adc4d615a9ddcaa027081e9a585577a753bfb78
markogalavic <55134792+markogalavic@users.noreply.github.com>
2020-10-13 10:40:04 +0200

    Add files via upload

---
+++ markoGalavićNasljeđivanje/markoGalavićNasljeđivanje/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace markoGalavićNasljeđivanje
+{
+    class Program
+    {
+        interface IControl
+        {
+            void Metoda();
+            int Brojac { get; set; }
+        }
+      
+        class ComboBox : IControl
+        {
+            private int brojac;
+
+            public int Brojac { get => brojac; set => brojac = value; }
+
+            public void Metoda()
+            {
+                Console.WriteLine("Metoda kombo");
+            }
+        }
+        class ListBox : IControl
+        {
+            private int brojac;
+
+            public int Brojac { get => brojac; set => brojac = value; }
+
+            public void Metoda()
+            {
+
+                Console.WriteLine("Metoda list");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            ListBox x = new ListBox();
+            ComboBox y = new ComboBox();
+            x.Metoda();
+            y.Metoda();
+            Console.ReadKey();
+
+        }
+
+    }
+    }
+
